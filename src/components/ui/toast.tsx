9054058ec63d3ace049438904393e4bd81322f58
Sanoy Das <sanoydasgemini@gmail.com>
2025-11-16 18:26:46 +0000

    place the position on the  bottom

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -16,7 +16,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed bottom-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-4 sm:right-4 sm:flex-col md:max-w-md",
+      "fixed bottom-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-4 sm:left-1/2 sm:-translate-x-1/2 sm:flex-col md:max-w-md",
       className
     )}
     {...props}
